4a6b69f3fcf8fe9e8d83cb20ef2c7c45d6ffcee0
Elise-Beauvy <94043023+Elise-Beauvy@users.noreply.github.com>
2022-11-24 14:23:22 +0100

    change

---
+++ serveur.py
@@ -0,0 +1,20 @@
+import socket
+
+msgsrv = ""
+msgcl = ""
+
+host = socket.gethostname()
+
+server_socket = socket.socket()
+server_socket.bind((host, 10000))
+server_socket.listen(1)
+while msgcl != "arret" and msgsrv != "arret":
+    conn, address = server_socket.accept()
+    while msgcl != "bye" and msgsrv != "bye" and msgcl != "arret" and msgsrv != "arret":
+
+        msgcl = conn.recv(1024).decode()
+        print(msgcl)
+        msgsrv = input("Isis:")
+        conn.send(msgsrv.encode())
+    conn.close()
+server_socket.close()
