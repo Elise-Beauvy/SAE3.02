ec3c9a33924959a271c49e0b0f319d45d800ff26
Elise-Beauvy <94043023+Elise-Beauvy@users.noreply.github.com>
2022-11-25 11:38:42 +0100

    change

--- client.py
+++ client.py
@@ -6,11 +6,10 @@ msgsrv = ""
 host = socket.gethostname()
 print(host)
 client_socket = socket.socket()
-client_socket.connect(("127.0.0.1",1024))
+client_socket.connect((host,1024))
 
-while msgcl != "bye" and msgsrv != "bye" and msgcl != "arret" and msgsrv != "arret":
-    msgcl = input("Amon:")
-    client_socket.send(msgcl.encode())
-    msgsrv = client_socket.recv(1024).decode()
-    print(msgsrv)
+msgcl = input("Amon:")
+client_socket.send(msgcl.encode())
+msgsrv = client_socket.recv(1024).decode()
+print(msgsrv)
 client_socket.close()
--- serveur.py
+++ serveur.py
@@ -6,15 +6,13 @@ msgcl = ""
 host = socket.gethostname()
 print(host)
 server_socket = socket.socket()
-server_socket.bind(("127.0.0.1",1024))
+server_socket.bind((host,1024))
 server_socket.listen(1)
-while msgcl != "arret" and msgsrv != "arret":
-    conn, address = server_socket.accept()
-    while msgcl != "bye" and msgsrv != "bye" and msgcl != "arret" and msgsrv != "arret":
+conn, address = server_socket.accept()
 
-        msgcl = conn.recv(1024).decode()
-        print(msgcl)
-        msgsrv = input("Isis:")
-        conn.send(msgsrv.encode())
-    conn.close()
+msgcl = conn.recv(1024).decode()
+print(msgcl)
+msgsrv = input("Isis:")
+conn.send(msgsrv.encode())
+conn.close()
 server_socket.close()
