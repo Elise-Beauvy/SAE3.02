3cbb23cdf58bfd1486addd4ffc70951f839d5364
Elise-Beauvy <94043023+Elise-Beauvy@users.noreply.github.com>
2022-11-25 09:46:23 +0100

    change

--- serveur.py
+++ serveur.py
@@ -6,7 +6,7 @@ msgcl = ""
 host = socket.gethostname()
 
 server_socket = socket.socket()
-server_socket.bind((host,10000))
+server_socket.bind(("192.168.43.3",10000))
 server_socket.listen(1)
 while msgcl != "arret" and msgsrv != "arret":
     conn, address = server_socket.accept()
