52c2039a2600fd93b4616444a481df1a90aaf2be
Elise-Beauvy <94043023+Elise-Beauvy@users.noreply.github.com>
2022-11-25 11:33:08 +0100

    change

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ msgsrv = ""
 host = socket.gethostname()
 print(host)
 client_socket = socket.socket()
-client_socket.connect(("0.0.0.0",10000))
+client_socket.connect(("127.0.0.1",80))
 
 while msgcl != "bye" and msgsrv != "bye" and msgcl != "arret" and msgsrv != "arret":
     msgcl = input("Amon:")
