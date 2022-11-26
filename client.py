c4e77922a7693fa32a4e713e63212d46ee9f28fa
Elise-Beauvy <94043023+Elise-Beauvy@users.noreply.github.com>
2022-11-26 11:59:54 +0100

    change

--- client.py
+++ client.py
@@ -8,9 +8,6 @@ print(host)
 client_socket = socket.socket()
 client_socket.connect((host,10000))
 
-hostname = socket.gethostbyaddr("192.168.43.2")
-print(hostname)
-
 msgcl = input("Amon:")
 client_socket.send(msgcl.encode())
 msgsrv = client_socket.recv(1024).decode()
